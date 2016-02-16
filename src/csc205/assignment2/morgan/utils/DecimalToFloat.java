1954578ee2437742f97e35116d6822de8547bb4e
buzzcogs <buzzcogs@chandler.home>
2016-02-16 11:39:17 -0500

    added stub methods to utility class

--- src/csc205/assignment2/morgan/utils/DecimalToFloat.java
+++ src/csc205/assignment2/morgan/utils/DecimalToFloat.java
@@ -12,5 +12,37 @@ package csc205.assignment2.morgan.utils;
  */
 public class DecimalToFloat 
 {
-
+     public static float convertStringToFloat(String strVal)
+     {
+         float retVal = 5.45f;
+         return retVal;
+     }
+     
+     public static double convertStringToDouble(String strVal)
+     {
+         double retVal = 5.67d;
+         return retVal;
+     }
+     
+     public static String convertSPBinaryIntsToString()
+     {
+         
+         return "11001100101010101010110101010101";
+     }
+     
+     public static String convertDPBinaryIntsToString()
+     {
+         
+         return "1100110010101010101011010101010111001100101010101010110101010101";
+     }
+     
+     public static String convertDPBinaryIntstoHexString()
+     {
+         return "0xef3432d2a2323423";
+     }
+     
+     public static String convertSPBinaryIntstoHexString()
+     {
+         return "0xef2d2a23";
+     }
 }
